73a293df6ee1cb9dc466a5def3a7eee041f3faa9
Andrew Tran <tranandrew@gmail.com>
2021-08-23 10:30:57 -0500

    Adding tester js for questions

---
+++ Assets/script.js
@@ -0,0 +1,34 @@
+// creating an array pf question objects called questions.
+// prompts created. We don't want prompts. Will have to figure this out.
+var questions = [
+    {
+    prompt: "Which dinosaur is the king of the dinosaurs?\n(a) T-Rex\n(b) Triceratops\n(c)Steggosaurus\ (d) None of the above",
+    answer: "a"
+
+},
+{
+    prompt: "Which dinosaur is the king of the dinosaurs?\n(a) T-Rex\n(b) Triceratops\n(c)Steggosaurus\ (d) None of the above",
+    answer: "a"
+
+},
+{
+    prompt: "Which dinosaur is the king of the dinosaurs?\n(a) T-Rex\n(b) Triceratops\n(c)Steggosaurus\ (d) None of the above",
+    answer: "a"
+
+},
+]
+var score = 0;
+
+//Basic for loop structure for questions. Questions will stop once question = greater than question length
+for(var i=0, i < questions.length; i++) {
+    var response = window.prompt(questions[i].prompt]);
+
+    //Did they get the answer right?
+    if (response == questions[i]) {
+        score++;
+        alert("That is correct!!!")
+    } else {
+        alert("Sorry, that is incorrect!")
+    }
+}
+alert("You got " + score + "/" + questions.length);
