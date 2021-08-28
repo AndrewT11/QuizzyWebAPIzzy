fee9962088373ad2ad638f967c5feb5a74ce99c0
Andrew Tran <tranandrew@gmail.com>
2021-08-27 23:23:09 -0500

    added declared variables, declared variable selectors

--- opening.js
+++ opening.js
@@ -1,1 +1,43 @@
 //creating questions array
+
+var score = 0;
+
+
+//Selectors
+var currentTime = document.querySelector("#currentTime");
+var questionBox = document.querySelector("#questionBox");
+var timer = document.querySelector("#timeStart");
+
+//Question Array to be populated
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        options:  ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within ______.",
+        options: ["quotes", "curly braces", "parentheses", "square brackets"],
+        answer: "brackets"
+
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ______.",
+        options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+
+    },
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        options: ["quotes", "curly braces", "parentheses", "commas"],
+        answer: "parenthesis"
+
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: ["JavaScript", "terminal / bash", "console.log", "for loops"],
+        answer: "console.log"
+
+    }
+];
