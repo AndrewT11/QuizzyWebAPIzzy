2158e16780c40d6c13ca45d5c223af5d36e732c2
Andrew Tran <tranandrew@gmail.com>
2021-08-29 15:59:59 -0500

    debug spelling errors, redo css styling

--- highScores.js
+++ highScores.js
@@ -2,14 +2,14 @@
 
 
 //declaring selector variables
-var goBack = ducoment.querySelector("#goBack");
+var goBack = document.querySelector("#goBack");
 var clearHighScores = document.querySelector("#clearHighScores")
 var highScores = document.querySelector("#highScores");
 
 //Event Listeners for two buttons on High Score page
 goBack.addEventListener("click", function () {
     window.location.replace("opening.html");
-console.log()
+
 });
 
 clearHighScores.addEventListener("click", function () {
@@ -17,14 +17,15 @@ clearHighScores.addEventListener("click", function () {
     location.reload();
 });
 
-var allScores = localStorage.getItem("allScores");
-allScores = JSON.parse(allScores);
+var topScores = localStorage.getItem("topScores");
+topScores = JSON.parse(topScores);
+
 
 //lists new top score to high score list
 if(topScores !== null) {
     for (var i = 0; i < topScores.length; i++) {
         var createLi = document.createElement("li");
-        createLi.textContent = topScores[i].initials + " " + alllScores[i].score;
+        createLi.textContent = topScores[i].initials + " " + topScores[i].score;
         highScores.appendChild(createLi);
     }
 };
